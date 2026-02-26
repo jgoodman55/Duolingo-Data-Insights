b4ba5aadeebf2b7451b8b5f0f59860934b012f3c
Jordan Goodman <jordscot@umich.edu>
2026-02-26 10:38:42 -0600

    adding user overall_accuracy

--- dbt/models/marts/dim_user.sql
+++ dbt/models/marts/dim_user.sql
@@ -8,8 +8,16 @@ user_stats as (
 
         MIN(dim_user_learning_language.first_activity_ts) AS first_activity_ts,
         MAX(dim_user_learning_language.last_activity_ts) AS last_activity_ts,
+
         SUM(dim_user_learning_language.total_practices) AS total_practices,
+
         COUNT(DISTINCT dim_user_learning_language.learning_language) AS cnt_learning_languages
+
+        CASE 
+            WHEN SUM(dim_user_learning_language.user_history_seen) > 0
+            THEN CAST(SUM(dim_user_learning_language.user_history_correct) AS FLOAT64)/SUM(dim_user_learning_language.user_history_seen)
+            ELSE NULL
+        END AS overall_accuracy
 
     FROM {{ ref('dim_user_learning_language') }} dim_user_learning_language
     GROUP BY dim_user_learning_language.user_id
