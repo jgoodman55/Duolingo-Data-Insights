bcc80543854051b169a06b30f752ff4ed1bd5457
Jordan Goodman <jordscot@umich.edu>
2026-02-26 10:49:32 -0600

    building first_activity_ts

--- dbt/models/marts/dim_user_learning_language.sql
+++ dbt/models/marts/dim_user_learning_language.sql
@@ -8,6 +8,9 @@ lexeme_accuracy AS (
         learning_language,
         lexeme_id, 
 
+        MIN(event_ts) as first_activity_ts,
+        MAX(event_ts) as last_activity_ts,
+
         MAX(history_correct) AS lexeme_history_correct, 
         MAX(history_seen) AS lexeme_history_seen
 
@@ -22,6 +25,9 @@ user_learning_language_accuracy AS (
     
         user_id, 
         learning_language,
+
+        MIN(first_activity_ts) AS first_activity_ts,
+        MAX(last_activity_ts) AS last_activity_ts,
         
         SUM(lexeme_history_correct) AS user_history_correct, 
         SUM(lexeme_history_seen) AS user_history_seen
@@ -48,8 +54,8 @@ user_learning_language_stats AS (
             ELSE NULL
         END AS overall_accuracy,
 
-        MIN(user_learning_language_accuracy.event_ts) AS first_activity_ts,
-        MAX(user_learning_language_accuracy.event_ts) AS last_activity_ts,
+        MIN(user_learning_language_accuracy.first_activity_ts) AS first_activity_ts,
+        MAX(user_learning_language_accuracy.last_activity_ts) AS last_activity_ts,
 
         COUNT(*) AS total_practices
 
