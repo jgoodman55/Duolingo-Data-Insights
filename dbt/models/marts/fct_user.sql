1c500d3172787edf9c8f6cd03e772f27ef7bbdec
Jordan Goodman <jordscot@umich.edu>
2026-02-26 12:03:46 -0600

    updating references to fct

--- dbt/models/marts/fct_user.sql
+++ dbt/models/marts/fct_user.sql
@@ -4,23 +4,23 @@ user_stats as (
 
     SELECT 
     
-        dim_user_learning_language.user_id, 
+        fct_user_learning_language.user_id, 
 
-        MIN(dim_user_learning_language.first_activity_ts) AS first_activity_ts,
-        MAX(dim_user_learning_language.last_activity_ts) AS last_activity_ts,
+        MIN(fct_user_learning_language.first_activity_ts) AS first_activity_ts,
+        MAX(fct_user_learning_language.last_activity_ts) AS last_activity_ts,
 
-        SUM(dim_user_learning_language.total_practices) AS total_practices,
+        SUM(fct_user_learning_language.total_practices) AS total_practices,
 
-        COUNT(DISTINCT dim_user_learning_language.learning_language) AS cnt_learning_languages,
+        COUNT(DISTINCT fct_user_learning_language.learning_language) AS cnt_learning_languages,
 
         CASE 
-            WHEN SUM(dim_user_learning_language.user_history_seen) > 0
-            THEN CAST(SUM(dim_user_learning_language.user_history_correct) AS FLOAT64)/SUM(dim_user_learning_language.user_history_seen)
+            WHEN SUM(fct_user_learning_language.user_history_seen) > 0
+            THEN CAST(SUM(fct_user_learning_language.user_history_correct) AS FLOAT64)/SUM(fct_user_learning_language.user_history_seen)
             ELSE NULL
         END AS overall_accuracy
 
-    FROM {{ ref('dim_user_learning_language') }} dim_user_learning_language
-    GROUP BY dim_user_learning_language.user_id
+    FROM {{ ref('fct_user_learning_language') }} fct_user_learning_language
+    GROUP BY fct_user_learning_language.user_id
 
 )
 
